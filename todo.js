95c32ac98bb1b8799bff38eb95719787cdd2f7d2
Gerald-Star <chimagerald3@gmail.com>
2023-08-13 06:37:05 +0200

    debugged errors on the lockr.js, debugged arrays to display items

--- todo.js
+++ todo.js
@@ -4,63 +4,63 @@ const itemsDisplay = document.getElementById("itemsDisplay");
 
 displayItems(getItems());
 
-//create the onclick function to be called when the item is clicked
-
 btnSave.onclick = function () {
   const toDoText = toDoItem.value;
   storeItem(toDoText);
 };
 
-//store the onclick function to be called when the item is clicked
+function storeItem(item) {
+  let items = getItems();
+  if (items != undefined && items != "") {
+    items = `${items} , ${item}`;
+  } else {
+    items = item;
+  }
+  saveToDos(items);
+}
 
-// function storeItem(toDoText) {
-//   let items = getItems();
-//   if (items != undefined && items != "") {
-//     items = `${items} , ${item}`;
-//   } else {
-//     items = item;
-//   }
+function saveToDos(items) {
+  Lockr.set("Items", items);
+  displayItems(items);
+  clearEntry();
+}
 
-//   saveToDos(items);
-// }
+function clearEntry() {
+  toDoItem.value = "";
+}
 
-// create a new function  saveToDos(items); to save the items to the database (local storage)
-//using the lockr.js framework to store the items in local storage
+function getItems() {
+  const todos = Lockr.get("Items");
+  return todos;
+}
 
-// function saveToDos(items) {
-//   Lockr.set("Items", items); //set the lockr.js framework to store the items
-//   displayItems(Items);
-//   clearEntry(); // clears the entry from the database
-// }
+function displayItems(items) {
+  if (items != undefined && items != "") {
+    const itemArray = items.split(",");
+    let out = '<ul class="list">';
+    for (var x = 0; x < itemArray.length; x++) {
+      out +=
+        ' <li class="list-item list-item--longdivider"><div class="list-item__center list-item--longdivider__center">';
+      out += "" + itemArray[x] + "</div>";
+      out +=
+        '<div class="list-item__right" onclick="deleteItem(' +
+        x +
+        ');"><i class="zmdi zmdi-delete"></i></div></li>';
+    }
+    out += "</ul>";
+    itemsDisplay.innerHTML = out;
+  }
+}
 
-//create a function to getItems()
-
-// function getItems() {
-//   const todos = Lockr.get("items");
-//   return todos;
-// }
-
-// //create a new entry function clearEntry() and set the toDoItem value
-
-// function clearEntry() {
-//   toDoItem.value = "";
-// }
-
-// //create a function to displayItems using array method()split
-// function displayItems(Items) {
-//   if (items != undefined && items != "") {
-//     const itemArray = items.split(",");
-//     let out = '<ul class="list">';
-//     for (var x = 0; x < itemArray.length; x++) {
-//       out +=
-//         '<li class="list-item list-item--longdivider"><div class="list-item_center list-item_longdivider-center">';
-//       out += "" + itemArray[x] + "</div >";
-//       out +=
-//         '<div class="list-item_right" onclick=deleteItem(' +
-//         x +
-//         ');"><i class="zmdi zmdi-delete"></i></div></li>';
-//     }
-//     out += "</ul >";
-//     itemsDisplay.innerHTML = out;
-//   }
-// }
+function deleteItem(itemToDelete) {
+  let items = getItems();
+  let itemArray = items.split(",");
+  let newItems = new Array();
+  for (var x = 0; x < itemArray.length; x++) {
+    if (x != itemToDelete) {
+      newItems.push(itemArray[x]);
+    }
+  }
+  displayItems(newItems.join());
+  saveToDos(newItems.join());
+}
